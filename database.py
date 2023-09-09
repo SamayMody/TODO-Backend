4eaabe7cd9741471a5805e7d9e38aad4dad0c3e1
Samay Mody <sm7525@srmist.edu.in>
2023-09-09 16:02:17 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,41 @@
+import pymongo
+
+MongoURI = "mongodb+srv://samaymody:Srmsam%4033@cluster0.ri5c637.mongodb.net/"
+client = pymongo.MongoClient(MongoURI)
+db = client["TODO_Application"]
+collection = db["Task"]
+
+def create(data):
+    data = dict(data)
+    response = collection.insert_one(data)
+    return str(response.inserted_id)
+
+def all():
+    response = collection.find({})
+    data = []
+    for i in response:
+        i["_id"] = str(i["_id"])
+        data.append(i)
+    return data
+
+def get_oneWholeDayTasks(condition):
+    response = collection.find({"date": condition})
+    tasks = [task for task in response]
+    for task in tasks:
+        task["_id"] = str(task["_id"])
+    return tasks
+
+def delete(condition):
+    response = collection.delete_one({"date" : condition})
+    return response.deleted_count
+
+def update(data):
+    data = dict(data)
+    response = collection.update_one(
+        {"date": data["date"]},
+        {"$set": {
+            "completed": data["completed"],
+        }}
+    )
+    return response.modified_count
+
---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+import database
+import modal
+
+app = FastAPI()
+
+@app.get("/all tasks")
+def get_all():
+    data = database.all()
+    return {"data" : data}
+
+@app.get("/todays tasks")
+def get_one(date: int):
+    data = database.get_oneWholeDayTasks(date)
+    return {"data":data}
+
+@app.post("/set tasks")
+def post(data:modal.Task):
+    id = database.create(data)
+    return {"inserted": True, "inserted_id": id}
+
+@app.put("/change status")
+def update(data:modal.Task):
+    data = database.update(data)
+    return {"updated": True, "data": data}
+
+@app.delete("/delete")
+def delete(date: int):
+    data = database.delete(date)
+    return {"deleted": True , "deleted_count": data}
+
+
+@app.get("/completion_percentage")
+def get_completion_percentage(date: int):
+    # Get all tasks for the specified date
+    tasks = database.get_oneWholeDayTasks(date)
+
+    if isinstance(tasks, list):
+        total_tasks = len(tasks)
+        completed_tasks = sum(1 for task in tasks if task.get("completion_status", False))
+
+        if total_tasks > 0:
+            percentage_completed = (completed_tasks / total_tasks) * 100
+        else:
+            percentage_completed = 0
+
+        return {"date": date, "completion_percentage": percentage_completed}
+    else:
+        return {"date": date, "completion_percentage": 0}
+
